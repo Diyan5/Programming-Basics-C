6e445936b57f367786b817b4992e4caa667c0fe6
Diyan5 <110912671+Diyan5@users.noreply.github.com>
2022-10-17 14:18:16 +0300

    Add files via upload

---
+++ 04. Inches to Centimeters/04. Inches to Centimeters.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _04._Inches_to_Centimeters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double inch = 2.54;
+            double izhod = a * inch;
+            Console.WriteLine(izhod);
+
+        }
+    }
+}

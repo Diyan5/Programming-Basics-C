c52dc4f4f91923c3b5995bd0f0c7fe669c6474da
Diyan5 <110912671+Diyan5@users.noreply.github.com>
2022-10-17 15:44:23 +0300

    Add files via upload

---
+++ 04. Sum of Two Numbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _04._Sum_of_Two_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingNumber = int.Parse(Console.ReadLine());
+            int endNuumber = int.Parse(Console.ReadLine());
+            int magicNumber = int.Parse(Console.ReadLine());
+            int combination = 0;
+            bool hasMagicNumberBeenFound = false;
+            for(int firstNumber=startingNumber;firstNumber<=endNuumber ;firstNumber++)
+            {
+                for(int secondnumber=startingNumber;secondnumber<=endNuumber;secondnumber++)
+                {
+                    combination++;
+                    if(firstNumber +secondnumber ==magicNumber )
+                    {
+                        hasMagicNumberBeenFound = true;
+                        Console.WriteLine($"Combination N:{combination } ({firstNumber } + {secondnumber } = {magicNumber})");
+                        break;
+                    }
+                }
+                if(hasMagicNumberBeenFound )
+                {
+                    break;
+                }
+            }
+            if(!hasMagicNumberBeenFound)
+            {
+                Console.WriteLine($"{combination } combinations - neither equals {magicNumber }");
+            }
+        }
+    }
+}
---
+++ 05. Travelling/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _05._Travelling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+            
+            while (destination != "End")
+            {
+               double budjet = double.Parse(Console.ReadLine());
+                double savedMoney = 0;
+                while (savedMoney<budjet)
+                {
+                    savedMoney += double.Parse(Console.ReadLine());
+
+
+                }
+                Console.WriteLine($"Going to {destination}!");
+                destination = Console.ReadLine();
+               
+                    
+                    
+                
+            }
+        }
+    }
+}

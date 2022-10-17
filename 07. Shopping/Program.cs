bc8d14b0bd3db3e7d59c71767a3ff5650ef1919e
Diyan5 <110912671+Diyan5@users.noreply.github.com>
2022-10-17 15:30:47 +0300

    Add files via upload

---
+++ 07. Shopping/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _07._Shopping
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budjet = double.Parse(Console.ReadLine());
+            int videokarti = int.Parse(Console.ReadLine());
+            int procesor = int.Parse(Console.ReadLine());
+            int ramPamet = int.Parse(Console.ReadLine());
+            //            Видеокарта – 250 лв./ бр.
+
+            //· Процесор – 35 % от цената на закупените видеокарти/ бр.
+
+            //· Рам памет – 10 % от цената на закупените видеокарти/ бр.
+                double a = videokarti * 250;
+            double b = a* 0.35;
+                double k = b * procesor;
+            double c = a * 0.1;
+                double m = c * ramPamet;
+            double d = a + k + m;
+            if ( videokarti>procesor)
+            {
+             d = (a + k + m) - ((a + k + m) * 0.15);
+            }
+                
+          if(budjet>=d)
+            {
+                double e = budjet - d;
+                Console.WriteLine($"You have {e:f2} leva left!");
+            }
+          else
+            {
+                double e = d - budjet;
+                Console.WriteLine($"Not enough money! You need {e:f2} leva more!");
+
+            }
+
+        }
+    }
+}
---
+++ 08. Lunch Break/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _08._Lunch_Break
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            int epizod = int.Parse(Console.ReadLine());
+            int pochivka = int.Parse(Console.ReadLine());
+            double obqd = pochivka * 0.125;
+            double otdih = pochivka * 0.25;
+            double a = pochivka - obqd - otdih;
+            if(a>=epizod)
+            {
+                double b = a - epizod;
+                Console.WriteLine($"You have enough time to watch {text} and left with {Math.Ceiling(b)} minutes free time.");
+            }
+            else
+            {
+                double b = epizod - a;
+                Console.WriteLine($"You don't have enough time to watch {text}, you need {Math.Ceiling(b)} more minutes.");
+            }
+        }
+    }
+}

2b831c1026bd0aa0d61423f66833edf3c483c5f9
Diyan5 <110912671+Diyan5@users.noreply.github.com>
2022-10-17 15:50:27 +0300

    Add files via upload

---
+++ 06. Max Number/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _06._Max_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int maxNumber = int.MinValue;
+            string name = Console.ReadLine();
+
+            while (name != "Stop")
+            {
+                int n = int.Parse(name);
+                if (n > maxNumber)
+                {
+                    maxNumber = n;
+                }
+                name = Console.ReadLine();
+
+            }
+            Console.WriteLine(maxNumber);
+        }
+    }
+}

6e4ea8ab6364d2b0fefbc1d54faac6de9728af7a
Diyan5 <110912671+Diyan5@users.noreply.github.com>
2022-10-17 15:53:37 +0300

    Add files via upload

---
+++ 01. Old Books/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _01._Old_Books
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string favouriteBook = Console.ReadLine();
+            
+            int counter = 0;
+            bool isBookFounf = false;
+            string nextBookName = Console.ReadLine();
+            
+            while(nextBookName  != "No More Books")
+            {
+                 
+                if (favouriteBook == nextBookName )
+                {
+                    isBookFounf = true;
+                    break;
+                }
+                counter++;
+                nextBookName = Console.ReadLine();
+                
+            }
+            if (isBookFounf)
+            {
+                Console.WriteLine($"You checked {counter} books and found it.");
+            }
+            else
+            {
+                Console.WriteLine($"The book you search is not here!\r\nYou checked {counter} books.");
+            }
+        }
+                
+    }
+}
---
+++ 03. Vacation/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _03._Vacation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double tripPrice = double.Parse(Console.ReadLine());
+            double totalMoney = double.Parse(Console.ReadLine());
+            int daysCounter = 0;
+            int spendingCounter = 0;
+            while(true)
+            {
+                string command = Console.ReadLine();
+                double currentMoney =double.Parse(Console.ReadLine());
+                daysCounter++;
+                if(command =="spend")
+                {
+                    spendingCounter++;
+                    totalMoney -= currentMoney;
+                    if(totalMoney <0)
+                    {
+                        totalMoney = 0;
+                    }
+                    if(spendingCounter ==5)
+                    {
+                        Console.WriteLine($"You can't save the money.");
+                        Console.WriteLine(daysCounter);
+                        break;
+                    }
+                    
+                }
+                else if (command == "save")
+                {
+                    spendingCounter = 0;
+                    totalMoney += currentMoney;
+                    if(totalMoney >=tripPrice )
+                    {
+                        Console.WriteLine($"You saved the money for {daysCounter } days.");
+                        break;
+                    }
+                }
+            }
+           
+        }
+    }
+}

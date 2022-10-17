f4fcfa415b7df619349158211f08bbe9daa7a4f9
Diyan5 <110912671+Diyan5@users.noreply.github.com>
2022-10-17 16:00:38 +0300

    Add files via upload

---
+++ 11. Fruit Shop/Program.cs
@@ -0,0 +1,231 @@
+﻿using System;
+
+namespace _11._Fruit_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fruit = Console.ReadLine();
+            string day = Console.ReadLine();
+            double quality = double.Parse(Console.ReadLine());
+            double totalSum = 0;
+//            плод banana apple orange grapefruit kiwi pineapple grapes
+
+            // цена 2.50 1.20 0.85 1.45 2.70 5.50 3.85
+            switch (day)
+            {
+                case "Monday":
+                    switch(fruit)
+                    {
+                        case "banana":
+                            totalSum = 2.50 * quality;
+                            break;
+                        case "apple":
+                            totalSum = 1.20 * quality;
+                            break;
+                        case "orange":
+                            totalSum = 0.85 * quality;
+                            break;
+                        case "grapefruit":
+                            totalSum = 1.45 * quality;
+                            break;
+                        case "kiwi":
+                            totalSum = 2.70 * quality;
+                            break;
+                        case "pineapple":
+                            totalSum = 5.50 * quality;
+                            break;
+                        case "grapes":
+                            totalSum = 3.85 * quality;
+                            break;
+                        default:
+                            Console.WriteLine("error");
+                            break;
+                    }
+                    break;
+                case "Tuesday":
+                    switch (fruit)
+                    {
+                        case "banana":
+                            totalSum = 2.50 * quality;
+                            break;
+                        case "apple":
+                            totalSum = 1.20 * quality;
+                            break;
+                        case "orange":
+                            totalSum = 0.85 * quality;
+                            break;
+                        case "grapefruit":
+                            totalSum = 1.45 * quality;
+                            break;
+                        case "kiwi":
+                            totalSum = 2.70 * quality;
+                            break;
+                        case "pineapple":
+                            totalSum = 5.50 * quality;
+                            break;
+                        case "grapes":
+                            totalSum = 3.85 * quality;
+                            break;
+                        default:
+                            Console.WriteLine("error");
+                            break;
+                    }
+                    break;
+                case "Wednesday":
+                    switch (fruit)
+                    {
+                        case "banana":
+                            totalSum = 2.50 * quality;
+                            break;
+                        case "apple":
+                            totalSum = 1.20 * quality;
+                            break;
+                        case "orange":
+                            totalSum = 0.85 * quality;
+                            break;
+                        case "grapefruit":
+                            totalSum = 1.45 * quality;
+                            break;
+                        case "kiwi":
+                            totalSum = 2.70 * quality;
+                            break;
+                        case "pineapple":
+                            totalSum = 5.50 * quality;
+                            break;
+                        case "grapes":
+                            totalSum = 3.85 * quality;
+                            break;
+                        default:
+                            Console.WriteLine("error");
+                            break;
+                    }
+                    break;
+                case "Thursday":
+                    switch (fruit)
+                    {
+                        case "banana":
+                            totalSum = 2.50 * quality;
+                            break;
+                        case "apple":
+                            totalSum = 1.20 * quality;
+                            break;
+                        case "orange":
+                            totalSum = 0.85 * quality;
+                            break;
+                        case "grapefruit":
+                            totalSum = 1.45 * quality;
+                            break;
+                        case "kiwi":
+                            totalSum = 2.70 * quality;
+                            break;
+                        case "pineapple":
+                            totalSum = 5.50 * quality;
+                            break;
+                        case "grapes":
+                            totalSum = 3.85 * quality;
+                            break;
+                        default:
+                            Console.WriteLine("error");
+                            break;
+                    }
+                    break;
+                case "Friday":
+                    switch (fruit)
+                    {
+                        case "banana":
+                            totalSum = 2.50 * quality;
+                            break;
+                        case "apple":
+                            totalSum = 1.20 * quality;
+                            break;
+                        case "orange":
+                            totalSum = 0.85 * quality;
+                            break;
+                        case "grapefruit":
+                            totalSum = 1.45 * quality;
+                            break;
+                        case "kiwi":
+                            totalSum = 2.70 * quality;
+                            break;
+                        case "pineapple":
+                            totalSum = 5.50 * quality;
+                            break;
+                        case "grapes":
+                            totalSum = 3.85 * quality;
+                            break;
+                        default:
+                            Console.WriteLine("error");
+                            break;
+                    }
+                    break;
+                case "Saturday":
+                    switch (fruit)
+                    {
+                        case "banana":
+                            totalSum = 2.70 * quality;
+                            break;
+                        case "apple":
+                            totalSum = 1.25 * quality;
+                            break;
+                        case "orange":
+                            totalSum = 0.90 * quality;
+                            break;
+                        case "grapefruit":
+                            totalSum = 1.60 * quality;
+                            break;
+                        case "kiwi":
+                            totalSum = 3.00 * quality;
+                            break;
+                        case "pineapple":
+                            totalSum = 5.60 * quality;
+                            break;
+                        case "grapes":
+                            totalSum = 4.20 * quality;
+                            break;
+                        default:
+                            Console.WriteLine("error");
+                            break;
+                    }
+                    break;
+                case "Sunday":
+                    switch (fruit)
+                    {
+                        case "banana":
+                            totalSum = 2.70 * quality;
+                            break;
+                        case "apple":
+                            totalSum = 1.25 * quality;
+                            break;
+                        case "orange":
+                            totalSum = 0.90 * quality;
+                            break;
+                        case "grapefruit":
+                            totalSum = 1.60 * quality;
+                            break;
+                        case "kiwi":
+                            totalSum = 3.00 * quality;
+                            break;
+                        case "pineapple":
+                            totalSum = 5.60 * quality;
+                            break;
+                        case "grapes":
+                            totalSum = 4.20 * quality;
+                            break;
+                        default:
+                            Console.WriteLine("error");
+                            break;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("error");
+                    break;
+            }
+            if (totalSum > 0)
+            {
+                Console.WriteLine($"{totalSum:f2}");
+            }
+        }
+    }
+}
---
+++ 12. Trade Commissions/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace _12._Trade_Commissions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string town = Console.ReadLine();
+            double sales = double.Parse(Console.ReadLine());
+            double totalSum = 0;
+            switch(town)
+            {
+                case "Sofia":
+                    if(sales >=0 && sales <=500)
+                    {
+                        totalSum = sales * 0.05;
+                    }
+                    else if(sales >= 500 && sales <= 1000)
+                    {
+                        totalSum = sales * 0.07;
+                    } 
+                    else if(sales >= 1000 && sales <= 10000)
+                    {
+                        totalSum = sales * 0.08;
+                    }
+                    else if(sales >10000)
+                    {
+                        totalSum = sales * 0.12;
+                    
+                   }
+                    else
+                    {
+                        Console.WriteLine("error");
+                    }
+                    break;
+                case "Varna":
+                    if (sales >= 0 && sales <= 500)
+                    {
+                        totalSum = sales * 0.045;
+                    }
+                    else if (sales >= 500 && sales <= 1000)
+                    {
+                        totalSum = sales * 0.075;
+                    }
+                    else if (sales >= 1000 && sales <= 10000)
+                    {
+                        totalSum = sales * 0.10;
+                    }
+                    else if(sales >10000)
+                    {
+                        totalSum = sales * 0.13;
+                    }
+                    else
+                    {
+                        Console.WriteLine("error");
+                    }
+                    break;
+                case "Plovdiv":
+                    if (sales >= 0 && sales <= 500)
+                    {
+                        totalSum = sales * 0.055;
+                    }
+                    else if (sales >= 500 && sales <= 1000)
+                    {
+                        totalSum = sales * 0.08;
+                    }
+                    else if (sales >= 1000 && sales <= 10000)
+                    {
+                        totalSum = sales * 0.12;
+                    }
+                    else if(sales >10000)
+                    {
+                        totalSum = sales * 0.145;
+                    }
+                    else
+                    {
+                        Console.WriteLine("error");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("error");
+                    break;
+                   
+            }
+            if (totalSum > 0)
+            {
+                Console.WriteLine($"{totalSum:f2}");
+            }
+
+
+
+        }
+    }
+}

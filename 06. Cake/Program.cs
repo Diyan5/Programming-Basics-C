7b0c930475d7af05de96964aba9fc01e631f07f4
Diyan5 <110912671+Diyan5@users.noreply.github.com>
2022-10-17 15:55:21 +0300

    Add files via upload

---
+++ 06. Cake/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _06._Cake
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int shirina = int.Parse(Console.ReadLine());
+            int duljina = int.Parse(Console.ReadLine());
+            int sum = shirina * duljina;
+            while(sum>0)
+            {
+                string command = Console.ReadLine();
+                if(command =="STOP")
+                {
+                    Console.WriteLine($"{sum} pieces are left.");
+                    break;
+                }
+                else
+                {
+                    int parcheta = int.Parse(command );
+                    sum -= parcheta;
+                }
+               
+            }
+            if(sum<=0)
+            {
+                Console.WriteLine($"No more cake left! You need {Math.Abs (sum) } pieces more."); 
+            }
+        }
+    }
+}
---
+++ 07. Moving/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _07._Moving
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double shirina = double.Parse(Console.ReadLine());
+            double duljina = double.Parse(Console.ReadLine());
+            double vissochina = double.Parse(Console.ReadLine());
+            double cubm = Math.Floor(shirina * duljina * vissochina);
+            while(cubm >0)
+            {
+                string command = Console.ReadLine();
+                if(command == "Done")
+                {
+                    Console.WriteLine($"{cubm} Cubic meters left.");
+                    break;
+                }
+                else
+                {
+                    int cubmk = int.Parse(command);
+                    cubm -= cubmk;
+                }
+            }
+            if(cubm<=0)
+            {
+                Console.WriteLine($"No more free space! You need {Math.Abs (cubm)} Cubic meters more.");
+            }
+        }
+    }
+}

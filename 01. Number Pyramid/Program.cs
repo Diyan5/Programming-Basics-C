1da9471c5ae3d79e8f594dc7c4dfd452c2afc8bc
Diyan5 <110912671+Diyan5@users.noreply.github.com>
2022-10-17 15:45:44 +0300

    Add files via upload

---
+++ 01. Number Pyramid/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _01._Number_Pyramid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int current = 1;
+            bool isBigger = false;
+            for(int i=1;i<=n; i++)
+            {
+                for(int a=1;a<=i;a++)
+                {
+                    if(current >n)
+                    {
+                        isBigger = true;
+                        break;
+                    }
+                    Console.Write(current + " ");
+                    current++;
+                }
+                if(isBigger )
+                {
+                    break;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ 02. Equal Sums Even Odd Position/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _02._Equal_Sums_Even_Odd_Position
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+            for(int i=start;i<= end;i++ )
+            {
+                string number = i.ToString();
+                int oddSum = 0;
+                int evenSum = 0;
+                for(int j=0;j<=number.Length; j++ )
+                {
+                    int currentDigit = int.Parse(number[j].ToString());
+                    if(j%2==0)
+                    {
+                        oddSum += currentDigit; 
+                    }
+                    else
+                    {
+                        evenSum += currentDigit; 
+                    }
+                }
+                if(oddSum ==evenSum )
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+    }
+}

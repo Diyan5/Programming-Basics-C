c093c13cfb0fe798aa7a545754cd99570b9d960d
Diyan5 <110912671+Diyan5@users.noreply.github.com>
2022-10-17 16:04:57 +0300

    Add files via upload

---
+++ 08. On Time for the Exam/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace _08._On_Time_for_the_Exam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int houronExam = int.Parse(Console.ReadLine());
+            int minonExam = int.Parse(Console.ReadLine());
+            int arrivalonExam = int.Parse(Console.ReadLine());
+            int minArrivalonExam = int.Parse(Console.ReadLine());
+            int examSuminMinutes = houronExam * 60 + minonExam;
+            int arriveSuminMinutes = arrivalonExam * 60 +minArrivalonExam;
+            if(arriveSuminMinutes>examSuminMinutes)
+            {
+                Console.WriteLine("Late");
+                if(arriveSuminMinutes - examSuminMinutes<60)
+                {
+                    Console.WriteLine($"{arriveSuminMinutes - examSuminMinutes} minutes after the start");
+                }
+                else
+                {
+                    int hours = (arriveSuminMinutes - examSuminMinutes) / 60;
+                    int minutes = (arriveSuminMinutes - examSuminMinutes) % 60;
+                    Console.WriteLine($"{hours}:{minutes:D2} hours after the start");
+                }
+            }
+            else if(arriveSuminMinutes == examSuminMinutes ||  examSuminMinutes - arriveSuminMinutes<=30)
+            {
+                Console.WriteLine("On time");
+                if(arriveSuminMinutes- examSuminMinutes !=0)
+                {
+                   
+                    Console.WriteLine($"{examSuminMinutes-arriveSuminMinutes} minutes before the start");
+                }
+            }
+            else if( examSuminMinutes - arriveSuminMinutes > 30)
+            {
+                Console.WriteLine("Early");
+                if(Math.Abs(examSuminMinutes - arriveSuminMinutes) <60)
+                {
+                    Console.WriteLine($"{examSuminMinutes - arriveSuminMinutes:D2} minutes before the start");
+                }
+                else
+                {
+                    
+                    int hours = (examSuminMinutes - arriveSuminMinutes) / 60;
+                    int minutes = (examSuminMinutes - arriveSuminMinutes) % 60;
+                    Console.WriteLine($"{hours}:{minutes:D2} hours before the start");
+                }
+            }
+        }
+    }
+}
---
+++ 09. Ski Trip/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace _09._Ski_Trip
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            string pomeshtenie = Console.ReadLine();
+            string ocenka = Console.ReadLine();
+            double a = 0.00;
+            switch(pomeshtenie)
+            {
+                case "room for one person":
+                    a = (days - 1) * 18;
+                    break;
+                case "apartment":
+                    
+                    if (days < 10)
+                    {
+                        double b = (days - 1) * 25;
+                        a = b - (b * 0.3);
+                    }
+                    else if (days >= 10 && days <= 15)
+                    {
+                        double b = (days - 1) * 25;
+                        a = b - (b * 0.35);
+                    }
+                    else if(days>15)
+                    {
+                        double b = (days - 1) * 25;
+                        a = b - (b * 0.5);
+                    }
+                    break;
+                case "president apartment":
+                    if (days < 10)
+                    {
+                        double b = (days - 1) * 35;
+                        a = b - (b * 0.1);
+                    }
+                    else if (days >= 10 && days <= 15)
+                    {
+                        double b = (days - 1) * 35;
+                        a = b - (b * 0.15);
+                    }
+                    else if (days > 15)
+                    {
+                        double b = (days - 1) * 35;
+                        a = b - (b * 0.2);
+                    }
+                    break;
+            }
+            switch(ocenka)
+            {
+                case "positive":
+                    a = a + (a * 0.25);
+                    break;
+                case "negative":
+                    a = a - (a * 0.1);
+                    break;
+            }
+            Console.WriteLine($"{a:f2}");
+        }
+    }
+}

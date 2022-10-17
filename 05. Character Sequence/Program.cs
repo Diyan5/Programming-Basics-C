e8425323d0b46ae74b3059741b7cab9ae1186a12
Diyan5 <110912671+Diyan5@users.noreply.github.com>
2022-10-17 15:34:22 +0300

    Add files via upload

---
+++ 05. Character Sequence/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _05._Character_Sequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int Lenght = name.Length;
+            for (int value=0; value<=name.Length-1;value++)
+            {
+                Console.WriteLine(name[value]);
+            }
+        }
+    }
+}
---
+++ 06. Vowels Sum/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _06._Vowels_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            int wordLenght = word.Length;
+            int sum = 0;
+            for(int value =0;value<=wordLenght-1;value++)
+            {
+                char letter = word[value];
+//                буква a e i o u
+
+//             стойност 1 2 3 4 5
+                switch (letter)
+                {
+                    case 'a':
+                        sum++;
+                        break;
+                    case 'e':
+                        sum += 2;
+                        break;
+                    case 'i':
+                        sum += 3;
+                        break;
+                    case 'o':
+                        sum += 4;
+                        break;
+                    case 'u':
+                        sum += 5;
+                        break;
+                    
+                }
+                
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}

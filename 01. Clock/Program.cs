c7166a7b3853857d0160d043ecbae793a500a823
Diyan5 <110912671+Diyan5@users.noreply.github.com>
2022-10-17 15:42:59 +0300

    Add files via upload

---
+++ 01. Clock/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01._Clock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int firstNumber = 0; firstNumber <=23; firstNumber++)
+            {
+                for(int secondNumber=0;secondNumber <=59;secondNumber++)
+                {
+                    Console.WriteLine($"{firstNumber }:{secondNumber }"); 
+                }
+            }
+        }
+    }
+}

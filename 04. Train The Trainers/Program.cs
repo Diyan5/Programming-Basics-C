5627f693b476e67605ebef4780a9526c300846b2
Diyan5 <110912671+Diyan5@users.noreply.github.com>
2022-10-17 15:47:30 +0300

    Add files via upload

---
+++ 04. Train The Trainers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _04._Train_The_Trainers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopleImjury = int.Parse(Console.ReadLine());
+            string nameofPresentation = Console.ReadLine();
+            double averageScoreForAllPresentation = 0;
+            int counter = 0;
+            while(nameofPresentation !="Finish")
+            {
+                double averageScore = 0;
+                for(int i=0;i<peopleImjury;i++ )
+                {
+                    double currentscore = double.Parse(Console.ReadLine());
+                    averageScore += currentscore;
+                    averageScoreForAllPresentation += currentscore;
+                    counter++;
+                }
+                double averageScorePerPresentation = averageScore / peopleImjury;
+                Console.WriteLine($"{nameofPresentation } - {averageScorePerPresentation:f2}.");
+                nameofPresentation = Console.ReadLine(); 
+            }
+            double assement = averageScoreForAllPresentation / counter;
+            Console.WriteLine($"Student's final assessment is {assement:f2}."); 
+        }
+    }
+}
---
+++ 06. Cinema Tickets/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace _06._Cinema_Tickets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameOfMovie = Console.ReadLine();
+            int studentCounter = 0;
+            int standardCounter = 0;
+            int kidCounter = 0;
+            while (nameOfMovie !="Finish")
+            {
+                int freePlaces = int.Parse(Console.ReadLine());
+                int counter = 0;
+                while(freePlaces >counter)
+                {
+                    string typeOfTicket = Console.ReadLine();
+                    if(typeOfTicket =="End")
+                    {
+                        break;
+                    }
+                    if(typeOfTicket =="student")
+                    {
+                        studentCounter++; 
+                    }
+                    else if(typeOfTicket =="standard")
+                    {
+                        standardCounter++; 
+                    }
+                    else if(typeOfTicket =="kid")
+                    {
+                        kidCounter++;
+                    }
+                    counter++;
+                }
+                double averagePlaces = counter * 100.0 / freePlaces;
+                Console.WriteLine($"{nameOfMovie } - {averagePlaces:f2}% full.");
+                nameOfMovie = Console.ReadLine(); 
+            }
+            int alltickets = studentCounter + standardCounter + kidCounter;
+            double percentStudentTickets = (double) studentCounter / alltickets * 100;
+
+            double perentStandartTickets = (double) standardCounter /alltickets * 100;
+            
+            double percentkidtickets = (double) kidCounter /alltickets * 100;
+            Console.WriteLine($"Total tickets: {alltickets}");
+            Console.WriteLine($"{percentStudentTickets :f2}% student tickets.");
+            Console.WriteLine($"{perentStandartTickets:f2}% standard tickets.");
+            Console.WriteLine($"{percentkidtickets:f2}% kids tickets.");
+
+        }
+    }
+}

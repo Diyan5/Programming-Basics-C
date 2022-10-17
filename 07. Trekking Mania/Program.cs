519ab5f371c31fea5d9de2e123c76e56a3eafbb7
Diyan5 <110912671+Diyan5@users.noreply.github.com>
2022-10-17 15:40:37 +0300

    Add files via upload

---
+++ 07. Trekking Mania/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _07._Trekking_Mania
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int grupi = int.Parse(Console.ReadLine());
+            int p1 = 0;
+            int p2 = 0;
+            int p3 = 0;
+            int p4 = 0;
+            int p5 = 0;
+            for (int i = 1; i<=grupi;i++)
+            {
+                int broiZaEdnaGrupa = int.Parse(Console.ReadLine());
+                if(broiZaEdnaGrupa<=5)
+                {
+                    p1 += broiZaEdnaGrupa;
+                }
+                else if (broiZaEdnaGrupa >= 6 && broiZaEdnaGrupa<=12)
+                {
+                    p2 += broiZaEdnaGrupa;
+                }
+                else if (broiZaEdnaGrupa >= 13&& broiZaEdnaGrupa<=25)
+                {
+                    p3 += broiZaEdnaGrupa;
+                }
+                else if (broiZaEdnaGrupa >= 26 && broiZaEdnaGrupa<=40)
+                {
+                    p4 +=broiZaEdnaGrupa;
+                }
+                else if(broiZaEdnaGrupa>=41)
+                {
+                    p5 += broiZaEdnaGrupa;
+                }
+            }
+            double a = p1 + p2 + p3 + p4 + p5;
+            Console.WriteLine($"{(p1*100/(a)):f2}%");
+            Console.WriteLine($"{(p2 * 100 / (a)):f2}%");
+            Console.WriteLine($"{(p3 * 100 / (a)):f2}%");
+            Console.WriteLine($"{(p4 * 100 / (a)):f2}%");
+            Console.WriteLine($"{(p5 * 100 / (a)):f2}%");
+        }
+    }
+}
---
+++ 08. Tennis Ranklist/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _08._Tennis_Ranklist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int broiTurniri = int.Parse(Console.ReadLine());
+            int nachalenBroiTochki = int.Parse(Console.ReadLine());
+            int finaelniTochki = 0;
+            double count = 0;
+            for(int i=1;i<=broiTurniri; i++)
+            {
+                string turnir = Console.ReadLine();
+                if(turnir=="W")
+                 {
+                    finaelniTochki += 2000;
+                    count++;
+                  }
+            else if (turnir == "F")
+                 {
+                    finaelniTochki += 1200;
+                }
+            else if (turnir == "SF")
+                 {
+                    finaelniTochki += 720;
+                }
+             }
+            double avarageTochki = finaelniTochki / broiTurniri;
+            double sum = finaelniTochki + nachalenBroiTochki;
+            double a = (count* 100)/broiTurniri;
+            Console.WriteLine($"Final points: {sum}\r\nAverage points: {Math.Floor(avarageTochki)}\r\n{a:f2}%");
+        }
+    }
+}

f3812f94e96e954646e3d27d477bbc9d197b404b
Diyan5 <110912671+Diyan5@users.noreply.github.com>
2022-10-17 15:59:25 +0300

    Add files via upload

---
+++ 05. Small Shop/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace _05._Small_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string product = Console.ReadLine();
+            string town = Console.ReadLine();
+            double quality = double.Parse(Console.ReadLine());
+            double totalSum = 0;
+//            град / продукт coffee water beer sweets peanuts
+
+                     //Sofia 0.50 0.80 1.20 1.45 1.60
+
+                     //Plovdiv 0.40 0.70 1.15 1.30 1.50
+
+                     //Varna 0.45 0.70 1.10 1.35 1.55
+            switch (town)
+            {
+                case "Sofia":
+                    switch (product)
+                    {
+                        case "coffee":
+                            totalSum = 0.50 * quality;
+                            break;
+                        case "water":
+                            totalSum = 0.80 * quality;
+                            break;
+                        case "beer":
+                            totalSum = 1.20 * quality;
+                            break;
+                        case "sweets":
+                            totalSum = 1.45 * quality;
+                            break;
+                        case "peanuts":
+                            totalSum = 1.60 * quality;
+                            break;
+                    }
+                    break;
+                case "Plovdiv":
+                    switch (product)
+                    {
+                        case "coffee":
+                            totalSum = 0.40 * quality;
+                            break;
+                        case "water":
+                            totalSum = 0.70 * quality;
+                            break;
+                        case "beer":
+                            totalSum = 1.15 * quality;
+                            break;
+                        case "sweets":
+                            totalSum = 1.30 * quality;
+                            break;
+                        case "peanuts":
+                            totalSum = 1.50 * quality;
+                            break;
+                    }
+
+                    break;
+                case "Varna":
+                    switch (product)
+                    {
+                        case "coffee":
+                            totalSum = 0.45 * quality;
+                            break;
+                        case "water":
+                            totalSum = 0.70 * quality;
+                            break;
+                        case "beer":
+                            totalSum = 1.10 * quality;
+                            break;
+                        case "sweets":
+                            totalSum = 1.35 * quality;
+                            break;
+                        case "peanuts":
+                            totalSum = 1.65 * quality;
+                            break;
+                    }
+                    break;
+            }
+            Console.WriteLine(totalSum);
+        }
+    }
+}
---
+++ 07.Working Hours/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+namespace _07.Working_Hours
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            string day = Console.ReadLine();
+            switch(day)
+            {
+                case "Monday":
+                    switch(hour)
+                    {
+                        case 10:
+                        case 11:
+                        case 12:
+                        case 13:
+                        case 14:
+                        case 15:
+                        case 16:
+                        case 17:
+                        case 18:
+                            Console.WriteLine("open");
+                            break;
+                        default:
+                            Console.WriteLine("closed");
+                            break;
+                    }
+                    break;
+                case "Tuesday":
+                    switch (hour)
+                    {
+                        case 10:
+                        case 11:
+                        case 12:
+                        case 13:
+                        case 14:
+                        case 15:
+                        case 16:
+                        case 17:
+                        case 18:
+                            Console.WriteLine("open");
+                            break;
+                        default:
+                            Console.WriteLine("closed");
+                            break;
+                    }
+                    break;
+                case "Wednesday":
+                    switch (hour)
+                    {
+                        case 10:
+                        case 11:
+                        case 12:
+                        case 13:
+                        case 14:
+                        case 15:
+                        case 16:
+                        case 17:
+                        case 18:
+                            Console.WriteLine("open");
+                            break;
+                        default:
+                            Console.WriteLine("closed");
+                            break;
+                    }
+                    break;
+                case "Thursday":
+                    switch (hour)
+                    {
+                        case 10:
+                        case 11:
+                        case 12:
+                        case 13:
+                        case 14:
+                        case 15:
+                        case 16:
+                        case 17:
+                        case 18:
+                            Console.WriteLine("open");
+                            break;
+                        default:
+                            Console.WriteLine("closed");
+                            break;
+                    }
+                    break;
+                case "Friday":
+                    switch (hour)
+                    {
+                        case 10:
+                        case 11:
+                        case 12:
+                        case 13:
+                        case 14:
+                        case 15:
+                        case 16:
+                        case 17:
+                        case 18:
+                            Console.WriteLine("open");
+                            break;
+                        default:
+                            Console.WriteLine("closed");
+                            break;
+                    }
+                    break;
+                case "Saturday":
+                    switch (hour)
+                    {
+                        case 10:
+                        case 11:
+                        case 12:
+                        case 13:
+                        case 14:
+                        case 15:
+                        case 16:
+                        case 17:
+                        case 18:
+                            Console.WriteLine("open");
+                            break;
+                        default:
+                            Console.WriteLine("closed");
+                            break;
+                    }
+                    break;
+                case "Sunday":
+                    switch(hour)
+                    {
+                        default:
+                            Console.WriteLine("closed");
+                            break;
+                    }
+                    break;
+            }
+        }
+    }
+}

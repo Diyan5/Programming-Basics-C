6d3d6890eeb0c06b8c0757dec707d7d333c51b93
Diyan5 <110912671+Diyan5@users.noreply.github.com>
2022-10-17 15:23:31 +0300

    Add files via upload

---
+++ 01. Excellent Result/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01._Excellent_Result
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double otlichen = 5.5;
+            double segashna = double.Parse(Console.ReadLine());
+            bool isCorrect = segashna >= otlichen;
+            if(isCorrect)
+            {
+                Console.WriteLine("Excellent!");
+            }
+        }
+    }
+}

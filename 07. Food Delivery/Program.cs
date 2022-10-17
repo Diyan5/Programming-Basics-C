06f78f164c5372b7cf8c4081efe578681cd66c7f
Diyan5 <110912671+Diyan5@users.noreply.github.com>
2022-10-17 14:28:00 +0300

    Add files via upload

---
+++ 07. Food Delivery/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _07._Food_Delivery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pileshko = int.Parse(Console.ReadLine());
+            int riba = int.Parse(Console.ReadLine());
+            int vegetariansko = int.Parse(Console.ReadLine());
+            double cenaPileshko = 10.35 ;
+            double cenaRiba =12.40 ;
+            double cenaVegetariansko = 8.15;
+            double obshtaCena = 10.35 * pileshko + 12.40 * riba + 8.15 * vegetariansko;
+            double desert = obshtaCena * 0.2;
+            double sum = obshtaCena + desert + 2.50;
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ 09. Fish Tank/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _09._Fish_Tank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int duljina = int.Parse(Console.ReadLine());
+            int shirina = int.Parse(Console.ReadLine());
+            int visochina = int.Parse(Console.ReadLine());
+            double procent = double.Parse(Console.ReadLine());
+            double obem = duljina * shirina * visochina;
+            double obemLitri = obem * 0.001;
+            double prostranstvo = procent * 0.01;
+            double sum = obemLitri * (1 - prostranstvo);
+            Console.WriteLine(sum);
+        }
+    }
+}

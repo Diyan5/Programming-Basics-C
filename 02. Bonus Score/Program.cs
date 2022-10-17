133a8888b3ba4d5598af8ba5ee957732a54fe3d1
Diyan5 <110912671+Diyan5@users.noreply.github.com>
2022-10-17 15:27:42 +0300

    Add files via upload

---
+++ 02. Bonus Score/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _02._Bonus_Score
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int chislo = int.Parse(Console.ReadLine());
+            double bonus = 0.0;
+            if(chislo<=100)
+            {
+                bonus = 5;
+                
+            }
+            else if (chislo>1000)
+            {
+                bonus = chislo *0.1;
+                
+            }
+            else
+            {
+                bonus = chislo *0.2;
+               
+            }
+            if(chislo%2 ==0)
+            {
+                bonus = bonus + 1;
+            }
+            else if( chislo%10 ==5)
+            {
+                bonus += 2;
+            }
+            Console.WriteLine(bonus);
+            Console.WriteLine(chislo + bonus);
+        }
+    }
+}
---
+++ 03. Time + 15 Minutes/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _03._Time___15_Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int chas = int.Parse(Console.ReadLine());
+            int min = int.Parse(Console.ReadLine());
+             min = min + 15;
+            if (min >= 60)
+            {
+                min = min - 60;
+                chas += 1;
+            }
+            if(chas==24)
+            {
+                chas = 0;
+            }
+            if(min<10)
+            {
+                Console.WriteLine($"{chas}:0{min}");
+            }
+            else
+            {
+                Console.WriteLine($"{chas}:{min}");
+            }
+        }
+    }
+}

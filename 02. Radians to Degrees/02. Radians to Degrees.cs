cfa36dfb409ad134b0430cf72893793d2df7e3ac
Diyan5 <110912671+Diyan5@users.noreply.github.com>
2022-10-17 14:24:14 +0300

    Add files via upload

---
+++ 02. Radians to Degrees/02. Radians to Degrees.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace _02._Radians_to_Degrees
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double radian = double.Parse(Console.ReadLine());
+            double gradus = radian * 180 / Math.PI;
+            Console.WriteLine(gradus);
+        }
+    }
+}
---
+++ 03. Deposit Calculator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _03._Deposit_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double dpSuma = double.Parse(Console.ReadLine());
+            int srok = int.Parse(Console.ReadLine());
+            double procent = double.Parse(Console.ReadLine());
+            double lihva = dpSuma * procent / 100;
+            double mesec = lihva / 12;
+            double krai = dpSuma + srok * mesec;
+            Console.WriteLine(krai);
+        }
+    }
+}

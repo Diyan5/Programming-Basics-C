b2c4e3f2cf503bf88c731d1eb5bc82bb4f2c1071
Diyan5 <110912671+Diyan5@users.noreply.github.com>
2022-10-17 15:49:08 +0300

    Add files via upload

---
+++ 02. Password/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _02._Password
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string password = Console.ReadLine();
+            string currentPassword = "";
+            while(password !=currentPassword )
+            {
+                currentPassword = Console.ReadLine();
+                
+            }
+            Console.WriteLine($"Welcome {name}!"); 
+        }
+    }
+}
---
+++ 03. Sum Numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _03._Sum_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            while(n>sum)
+            {
+                int a = int.Parse(Console.ReadLine());
+                sum += a;
+            }
+            Console.WriteLine(sum); 
+        }
+    }
+}

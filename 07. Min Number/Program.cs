2f36b845d8504920a00f25e134fd7046fdddc8aa
Diyan5 <110912671+Diyan5@users.noreply.github.com>
2022-10-17 15:51:24 +0300

    Add files via upload

---
+++ 07. Min Number/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _07._Min_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int minNumber = int.MaxValue;
+            string input = Console.ReadLine();
+            while(input!="Stop")
+            {
+                int currentNumber = int.Parse(input);
+                if (currentNumber < minNumber) 
+                {
+                    minNumber=currentNumber;
+                }
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(minNumber);
+        }
+    }
+}
---
+++ 08. Graduation/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _08._Graduation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameOfStudent = Console.ReadLine();
+            double rating = double.Parse(Console.ReadLine()); 
+            double totalRating = 0;
+            double avglRating = 0;
+            int grade = 1;
+            int fails = 0;
+            while(grade<=12)
+            {
+                
+                if(rating <4)
+                {
+                    fails++;
+                   
+                }
+                if (fails==2)
+                {
+                    grade--;
+                    Console.WriteLine($"{nameOfStudent} has been excluded at {grade} grade");
+                    break;
+                }
+                totalRating += rating;
+                avglRating = totalRating / grade;
+                grade++;
+               if(grade>12)
+                {
+                    Console.WriteLine($"{nameOfStudent} graduated. Average grade: {avglRating:f2}");
+                    break;
+                }
+                rating = double.Parse(Console.ReadLine());
+            }
+            
+
+            
+
+        }
+
+    }
+}
